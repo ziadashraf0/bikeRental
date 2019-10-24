c844f203f3e2ffbb1f3d46864966faa3c8f20b0c
ziadashraf0 <ziadshreet@gmail.com>
2019-10-24 14:05:03 +0200

    Dashboard

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -28,98 +28,103 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="homeContainer">
-        <div className="bar bg-dark border border-dark float-left p-2">
-          <span>
-            <h1 className="font-italic font-weight-bold text-primary">
-              BikeRental
-            </h1>
-          </span>
+      <body id="homeContainer">
+        <div className="homeContainer">
+          <div className="bar bg-dark border border-dark float-left p-2">
+            <span>
+              <h1 className="font-italic font-weight-bold text-primary">
+                BikeRental
+              </h1>
+            </span>
 
-          <ul>
-            <li className=" text-danger">
-              <a
-                className="text-danger"
-                onClick={() => this.Dashboard()}
-                href=""
-              >
-                Dashboard
-              </a>
-            </li>
-            <li className=" text-info">
-              <a
-                className="text-warning"
-                onClick={() => this.SignOut()}
-                href=""
-              >
-                sign out
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="handle">
-          <div className="confirmOwner ">
-            <button
-              className="btn btn-primary m-2 dropdown-toggle"
-              id="dropdownMenuButton"
-              onClick={this.openConfirmDialog}
-            >
-              Confirm Owner
-            </button>
+            <ul>
+              <li className=" text-danger">
+                <a
+                  className="text-danger"
+                  onClick={() => this.Dashboard()}
+                  href=""
+                >
+                  Dashboard
+                </a>
+              </li>
+              <li className=" text-info">
+                <a
+                  className="text-warning"
+                  onClick={() => this.SignOut()}
+                  href=""
+                >
+                  sign out
+                </a>
+              </li>
+            </ul>
           </div>
+          <div className="handle">
+            <div className="confirmOwner ">
+              <button
+                className="btn btn-primary m-2 dropdown-toggle"
+                id="dropdownMenuButton"
+                onClick={this.openConfirmDialog}
+              >
+                Confirm Owner
+              </button>
+            </div>
 
-          <div className="removeOwner">
-            <button
-              className="btn btn-danger m-2"
-              onClick={this.openRemoveDialog}
-            >
-              Remove Owner
-            </button>
+            <div className="removeOwner">
+              <button
+                className="btn btn-danger m-2"
+                onClick={this.openRemoveDialog}
+              >
+                Remove Owner
+              </button>
+            </div>
           </div>
-        </div>
-        {this.state.isConfirmDialogOpen && (
-          <dialog
-            className="border-primary bg-secondary "
-            modal={false}
-            open={true}
-          >
-            <form>
-              <input
-                type="number"
-                id="confirmSSN"
-                placeholder="Enter SSN to remove owner"
-              ></input>
-            </form>
-            <button
-              className="btn-danger m-2"
-              onClick={this.handleConfirmClose}
+          {this.state.isConfirmDialogOpen && (
+            <dialog
+              className="border-primary bg-secondary "
+              modal={false}
+              open={true}
             >
-              Confirm
-            </button>
-          </dialog>
-        )}
-        {this.state.isRemoveDialogOpen && (
-          <dialog
-            className="border-primary bg-secondary"
-            modal={false}
-            open={true}
-          >
-            <form>
-              <input
-                type="number"
-                id="removeSSN"
-                placeholder="Enter SSN to remove owner"
-              ></input>
-            </form>
-            <button className="btn-danger m-2" onClick={this.handleRemoveClose}>
-              Remove
-            </button>
-          </dialog>
-        )}
-        {/* <div className="users bg-primary ">
+              <form>
+                <input
+                  type="number"
+                  id="confirmSSN"
+                  placeholder="Enter SSN to remove owner"
+                ></input>
+              </form>
+              <button
+                className="btn-danger m-2"
+                onClick={this.handleConfirmClose}
+              >
+                Confirm
+              </button>
+            </dialog>
+          )}
+          {this.state.isRemoveDialogOpen && (
+            <dialog
+              className="border-primary bg-secondary"
+              modal={false}
+              open={true}
+            >
+              <form>
+                <input
+                  type="number"
+                  id="removeSSN"
+                  placeholder="Enter SSN to remove owner"
+                ></input>
+              </form>
+              <button
+                className="btn-danger m-2"
+                onClick={this.handleRemoveClose}
+              >
+                Remove
+              </button>
+            </dialog>
+          )}
+          {/* <div className="users bg-primary ">
           <span className="usersText font-italic font-weight-bold">Users</span>
         </div> */}
-      </div>
+        </div>
+      </body>
     );
   }
 }
