e50ec336d72809c30c23c5ece9231949dd3965a7
ziadashraf0 <ziadshreet@gmail.com>
2019-10-25 14:17:11 +0200

    adminEdit

---
+++ src/component/AdminEdit.jsx
@@ -0,0 +1,118 @@
+import React, { Component } from "react";
+import { directive } from "@babel/types";
+import "./AdimEdit.css";
+import login from "./Login";
+
+import Route from "react-router-dom/Route";
+import {
+    withRouter,
+    BrowserRouter as Router,
+    Link,
+    Switch,
+    Redirect
+} from "react-router-dom";
+class Edit extends Component {
+    state = {};
+
+    nextPath() {
+        let path = "/adminEdit";
+        this.props.history.push(path);
+    }
+
+    constructor() {
+        super();
+        this.state = {
+            email: null,
+            phonenum: null}
+        };
+
+        render(){
+            return (
+                <body className="register">
+                <div className="container">
+                    <h1 className="col-sm-6 offset-sm-3 text-center text-danger display-5">
+                        {" "}
+                        Edit Admin's page{" "}
+                    </h1>
+                    <br></br>
+                    <form className="form-inline">
+                        <div className="form-group mb-2">
+                            <label htmlFor="staticEmail2" className="sr-only">Email</label>
+                            <input type="text" readOnly className="form-control-plaintext" id="staticEmail2"
+                                   value="">
+                            </input>
+                        <div className="form-group mx-sm-3 mb-2">
+                            <label htmlFor="inputPassword2" className="sr-only">Password</label>
+                            <input type="password" className="form-control" id="inputPassword2" placeholder="Edit your email">
+                            </input>
+                        </div>
+                        <button type="submit" className="btn btn-primary mb-2 btn-danger">Edit</button>
+                        </div>
+                    </form>
+                    <form className="form-inline">
+                        <div className="form-group mb-2">
+                            <label htmlFor="staticEmail2" className="sr-only">Email</label>
+                            <input type="text" readOnly className="form-control-plaintext" id="staticEmail2"
+                                   value="">
+                            </input>
+                            <div className="form-group mx-sm-3 mb-2">
+                                <label htmlFor="inputPassword2" className="sr-only">Password</label>
+                                <input type="password" className="form-control" id="inputPassword2" placeholder="Edit your phone number">
+                                </input>
+                            </div>
+                            <button type="submit" className="btn btn-primary mb-2 btn-danger">Edit</button>
+                        </div>
+                    </form>
+                    <form className="form-inline">
+                        <div className="form-group mb-2">
+                            <label htmlFor="staticEmail2" className="sr-only">Email</label>
+                            <input type="text" readOnly className="form-control-plaintext" id="staticEmail2"
+                                   value="">
+                            </input>
+                            <div className="form-group mx-sm-3 mb-2">
+                                <label htmlFor="inputPassword2" className="sr-only">Password</label>
+                                <input type="password" className="form-control" id="inputPassword2" placeholder="Enter your old Password">
+                                </input>
+                            </div>
+                        </div>
+
+                    </form>
+                    <form className="form-inline">
+                        <div className="form-group mb-2">
+                            <label htmlFor="staticEmail2" className="sr-only">Email</label>
+                            <input type="text" readOnly className="form-control-plaintext" id="staticEmail2"
+                                   value="">
+                            </input>
+                            <div className="form-group mx-sm-3 mb-2">
+                                <label htmlFor="inputPassword2" className="sr-only">Password</label>
+                                <input type="password" className="form-control" id="inputPassword2" placeholder="Enter new password">
+                                </input>
+                            </div>
+                        </div>
+                    </form>
+                    <form className="form-inline">
+                        <div className="form-group mb-2">
+                            <label htmlFor="staticEmail2" className="sr-only">Email</label>
+                            <input type="text" readOnly className="form-control-plaintext" id="staticEmail2"
+                                   value="">
+                            </input>
+                            <div className="form-group mx-sm-3 mb-2">
+                                <label htmlFor="inputPassword2" className="sr-only">Password</label>
+                                <input type="password" className="form-control" id="inputPassword2" placeholder="Re-write new password">
+                                </input>
+                            </div>
+                            <button type="submit" className="btn btn-primary mb-2 btn-danger">Edit</button>
+                        </div>
+                    </form>
+
+
+
+
+
+                </div>
+                </body>
+            );
+        }
+
+}
+export default Edit;
