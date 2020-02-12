c1c5ffce0241ae83455a4d166a7412207b3f507a
ziadashraf0 <ziadshreet@gmail.com>
2020-02-12 16:19:28 +0200

    client and owner backend

--- BackEnd/Routes/client.js
+++ BackEnd/Routes/client.js
@@ -16,16 +16,58 @@ router.post("/IDsearch", async (req, res) => {
 
   return res.status(200).send(result);
 });
-router.post("/", async (req, res) => {
-  console.log(req.body);
-  const client = new Client({
+router.post("/signup", async (req, res) => {
+  const result = await Client.find({
+    $or: [{ SSN: req.body.SSN }, { email: req.body.email }]
+  });
+  if (result.length > 0) {
+    console.log("already exists");
+    return res.status(400).send("Already  Exists");
+  }
+  let client = new Client({
     SSN: parseInt(req.body.SSN),
     email: req.body.email,
+    password: req.body.password,
     firstName: req.body.firstName,
     lastName: req.body.lastName,
-    phoneNumber: req.body.phoneNumber
+    phoneNumber: req.body.phoneNumber,
+    birthDate: req.body.birthDate
   });
-  const result = await client.save();
-  res.send(result);
+  try {
+    client = await client.save();
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(req.body);
+
+  res.status(200).send(result);
 });
+router.post("/login", async (req, res) => {
+  console.log(req.body);
+  result = await Client.find({
+    email: req.body.email,
+    password: req.body.password
+  });
+  if (result.length === 0) {
+    console.log("not found");
+    return res.status(404).send("client is not found");
+  }
+  console.log("AUTHORIZED");
+  return res.status(200).send("AUTHORIZED");
+});
+router.get("/viewProfile", async (req, res) => {
+  if (!req.body.SSN) {
+    res.status(400).send("Bad Request");
+    return;
+  }
+  const client = await Client.find({ SSN: req.body.SSN }).select({
+    password: 0
+  });
+  if (client.length < 1) {
+    res.status(404).send("client was not found");
+    return;
+  }
+  res.status(200).send(client[0]);
+});
+
 module.exports = router;
--- BackEnd/Routes/owner.js
+++ BackEnd/Routes/owner.js
@@ -40,5 +40,72 @@ router.post("/", async (req, res) => {
   const result = await owner.save();
   res.send(result);
 });
+router.post("/login", async (req, res) => {
+  if (!req.body) {
+    res.status(400).send("Request body is empty");
+    return;
+  }
 
+  console.log(req.body);
+  result = await Owner.find({ email: req.body.email });
+
+  //if not found
+  if (result.length === 0) {
+    console.log("not found");
+    return res.status(404).send("Owner is not found");
+  }
+  const hash = result[0].password;
+  const results = await bcrypt.compare(req.body.password, hash);
+  if (results === true) {
+    console.log("AUTHORIZED");
+    return res.status(200).send("AUTHORIZED");
+  } else {
+    console.log("Not found");
+    return res.status(404).send("Wrong Password");
+  }
+});
+
+router.post("/signup", async (req, res) => {
+  console.log(req.body);
+  //look for an Owner with the same name || SSN;
+  const result = await Owner.find({
+    $or: [{ SSN: req.body.SSN }, { email: req.body.email }]
+  });
+
+  if (result.length > 0) {
+    console.log("already exists");
+    return res.status(400).send("Already  Exists");
+  }
+  let owner = new Owner({
+    SSN: req.body.SSN,
+    email: req.body.email,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phoneNumber: req.body.phoneNumber,
+    birthDate: req.body.birthDate
+  });
+  try {
+    owner = await owner.save();
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.status(200).send(owner);
+  console.log(owner);
+});
+
+router.get("/viewProfile", async (req, res) => {
+  console.log(req.body);
+  if (!req.body.SSN) {
+    res.status(400).send("Bad Request");
+    return;
+  }
+  const owner = await Owner.find({ SSN: req.body.SSN }).select({ password: 0 });
+  if (owner.length < 1) {
+    res.status(404).send("Owner was not found");
+    return;
+  }
+  res.status(200).send(owner[0]);
+});
 module.exports = router;
